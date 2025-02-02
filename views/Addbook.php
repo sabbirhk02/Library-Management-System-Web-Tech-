db92b8f4eeda45ce537cd8e89ce1504fb177e0cc
sabbirhk02 <sabbir00x@gmail.com>
2025-02-03 02:04:56 +0600

    Update Addbook.php

--- views/Addbook.php
+++ views/Addbook.php
@@ -1,76 +1,108 @@
+
 <?php
-include_once("../controller/view_userCheck.php");
-include_once("../controller/userData.php");
-
-?>
-
-<!DOCTYPE html>
+$bok_id = $_POST['uname'];
+   
+        if($bok_id==true){
+          ?>
+        
 <html>
-<title>Add New Book</title>
-<body>
-    <center>
-        <table height=635 width=1080>
-            <tr height=60>
-                <td colspan="2"> 
-                    <table width = "800">
-                    <tr>  
-                        <td align="right">
-                        <h2>Logged in as || <?php echo $user['name'] ?></h2> 
-                        </td>    
-                    </tr>    
-                    </table>
-                </td>
-            </tr>
-            <tr>
-                <td  width="300">   
-                <ul>                   
-                   <li> <a href="librarian_dashboard.php">Liabrarian Dashboard</a></li>										                  
-                    <li> <a href="profile.php">Profile</a></li>
-                    <li> <a href="catalog.php">Catalogue</a></li> 
-					          <li> <a href="search.php">Search</a></li> 
-                    <li> <a href="event.php">Event</a></li>
-                    <li> <a href="account_setting.php">Account Settings</a></li>					  					
-                    <li> <a href="../controller/logout.php">Logout</a></li>
-                </ul>
-                </td>
-                <td> 
-                   <h2 align="center">Add Book</h2>
-                   <form  method="POST" action="../controller/AddbookCheck.php"enctype="">               
-                    <fieldset>
-                      <legend>Book Management</legend>
-                      <table height="120"> 
-                      <tr>
-                          <td>Bookid</td>
-                          <td>:<input type="INT" name="Bookid" value="" > <br></td>
-                      </tr>
-                      <tr>
-                          <td>Bookname</td>
-                          <td>:<input type="text" name="Bookname" value="" > <br></td>
-                      </tr>
-                      <tr>
-                          <td>Author</td>
-                          <td>:<input type="text" name="Author" value="" > <br></td>
-                      </tr>
-                      <tr>
-                          <td>Catagory</td>
-                          <td>:<input type="text" name="Catagory" value="" > <br></td>
-                      </tr>    
-                      </table><hr>
-                      <input type="submit" name="submit" value="submit"> 
-                      <a href="catalog_management.php">Back</a>
-                    </fieldset>
-                    </form>   
-                   
-                                      
-                </td>
-            </tr>
+<title></title>
+<head>
+<style>
+        .error {
+            color: red;
+        }
+    </style>   
+   <script>
+        function validate() {
+            var Book_ID = document.getElementsByName("Bookid")[0].value;
+            var Book_Name = document.getElementsByName("Bookname")[0].value;
+            var Author = document.getElementsByName("Author")[0].value;
+            var Genre= document.getElementsByName("Genre")[0].value;
+            var Quantity = document.getElementsByName("Quantity")[0].value;
+            var image = document.getElementsByName("image")[0].value;
             
-        </table>
-        <table>
-               <tr height=40>
-                <td align="center">Copyright © by Sabbir Hossain</td>
-            </tr>
-        </table>
-    </center>
+            var inputFile = document.getElementById('imageInput');
+            var errorSpan = document.getElementById('error');
+
+        
+            if (inputFile.files.length === 0) {
+                errorSpan.textContent = 'Please select an image file.';
+                return false;
+            }
+
+     
+            var fileName = inputFile.files[0].name;
+            var fileExtension = fileName.split('.').pop().toLowerCase();
+
+            var allowedExtensions = ['jpeg', 'png', 'gif'];
+
+           
+            if (allowedExtensions.indexOf(fileExtension) === -1) {
+                errorSpan.textContent = 'Invalid file type. Please select a valid image file.';
+                return false;
+            }
+
+       
+            errorSpan.textContent = '';
+
+           
+            return true;
+            if (Book_ID === "") {
+                alert("Please enter a BookID");
+                return false;
+            }
+
+            if (Book_Name === "") {
+                alert("Please enter Book Name");
+                return false;
+            }
+            if (Author === "") {
+                alert("Please enter Author");
+                return false;
+            }
+
+           
+            if (Genre === "") {
+                alert("Please enter Genre");
+                return false;
+            }
+
+     
+            if (Quantity === "" || isNaN(Quantity) || !Number.isInteger(parseFloat(Ouantity))) {
+                alert("Please enter a  integer for Quantity Number");
+                return false;
+            }
+         
+            return true;
+        }
+        
+    </script></head>
+<body>
+<h2>ADD Book</h2>
+<form  method="POST" action="../views/AddbooksCheck.php"enctype="multipart/form-data"onsubmit="return validate()">
+  <fieldset>
+  <legend>Book Management</legend>
+  
+            Bookid: <input type="varchar"  id="Bookid" name="Bookid" value="" > <br>
+            Bookname: <input type="text" id="Bookname"name="Bookname" value="" > <br>
+            Author: <input type="text" id="Author" name="Author" value="" > <br>
+            Genre:    <input type="text" id="Genre"  name="Genre" value="" > <br>
+            Quantity <input type="INT" id="Quantity"name="Quantity" value="" > <br>
+            <label for="imageInput">Select an image:</label>
+            <input type="file" id="imageInput" name="image" value="" accept="image/jpeg, image/gif, image/png" >
+            
+      
+        <span id="error" class="error"></span>
+        <br>
+      
+                     <input type="submit" name="submit" value="ADD BOOK">	
+	<a href="ViewBooks.php">BACK</a>
+  </fieldset>
+</form>
 </body>
 </html>
+<?php
+        }
+          ?>
+        
